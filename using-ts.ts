6dc0119babc6cd1af7d8da7ebee41b6ef10f27cc
Sai Surya Teja <saisuryateja3@gmail.com>
2023-12-25 19:20:21 +0530

    Added basic example

--- using-ts.ts
+++ using-ts.ts
@@ -1,1 +1,10 @@
-document.getElementById("");
+const num1 = document.getElementById("num1")! as HTMLInputElement;
+const num2 = document.getElementById("num2")! as HTMLInputElement;
+const button = document.querySelector("button")! as HTMLButtonElement;
+
+const add = (a: number, b: number) => {
+  return a + b;
+};
+button.addEventListener("click", () => {
+  console.log(add(+num1.value, +num2.value));
+});
